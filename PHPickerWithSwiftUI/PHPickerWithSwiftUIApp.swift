afaa8acd28aa9f430b67d6dc960497fb7480d183
Tsurumoto Kentaro <tsurumotokentarou@gmail.com>
2021-04-02 16:49:18 +0900

    Initial Commit

---
+++ PHPickerWithSwiftUI/PHPickerWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PHPickerWithSwiftUIApp.swift
+//  PHPickerWithSwiftUI
+//
+//  Created by 鶴本賢太朗 on 2021/04/02.
+//
+
+import SwiftUI
+
+@main
+struct PHPickerWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PHPickerWithSwiftUITests/PHPickerWithSwiftUITests.swift
@@ -0,0 +1,33 @@
+//
+//  PHPickerWithSwiftUITests.swift
+//  PHPickerWithSwiftUITests
+//
+//  Created by 鶴本賢太朗 on 2021/04/02.
+//
+
+import XCTest
+@testable import PHPickerWithSwiftUI
+
+class PHPickerWithSwiftUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
